adad92580ef9b12c16392320bf00e0b484056292
fdepablo <f.depablo.lobo@gmail.com>
2024-02-15 09:30:48 +0100

    Hola mundo

--- RelacionesJava/src/git/MainGitBuenasPracticas.java
+++ RelacionesJava/src/git/MainGitBuenasPracticas.java
@@ -3,7 +3,7 @@ package git;
 public class MainGitBuenasPracticas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Bienvenidos al proyecto");
 
 	}
 
