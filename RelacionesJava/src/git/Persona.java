c570cbcb392c58d7e06dd157568399ae8fc55e31
fdepablo <f.depablo.lobo@gmail.com>
2024-02-15 10:36:37 +0100

    cambios en la rama main para probar las clases

--- RelacionesJava/src/git/Persona.java
+++ RelacionesJava/src/git/Persona.java
@@ -3,13 +3,37 @@ import java.util.Arrays;
 
 public class Persona {
 	private String nombre;
-
+	private Direccion direccion;
+	private CuentaBancaria[] cuentasBancarias;
+	
 	public String getNombre() {
 		return nombre;
 	}
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	public Direccion getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+
+	public CuentaBancaria[] getCuentasBancarias() {
+		return cuentasBancarias;
+	}
+
+	public void setCuentasBancarias(CuentaBancaria[] cuentasBancarias) {
+		this.cuentasBancarias = cuentasBancarias;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", direccion=" + direccion + ", cuentasBancarias="
+				+ Arrays.toString(cuentasBancarias) + "]";
 	}
 
 
