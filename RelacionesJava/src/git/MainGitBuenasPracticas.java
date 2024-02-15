590dcdfc63811896428d66bdca9967c812d45c1a
dgallegoa <danigallegoarense@gmail.com>
2024-02-15 10:29:32 +0100

    Añadido objeto en clase direccion

--- RelacionesJava/src/git/MainGitBuenasPracticas.java
+++ RelacionesJava/src/git/MainGitBuenasPracticas.java
@@ -4,6 +4,9 @@ public class MainGitBuenasPracticas {
 
 	public static void main(String[] args) {
 		System.out.println("Bienvenidos al proyecto");
+		
+		Direccion d1 = new Direccion();
+		d1.setCiudad("Madrid");
 
 	}
 
